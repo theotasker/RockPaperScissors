369bd78aee3a2eeb656612101dbc5d4af9779d7c
Theo Tasker <theotasker@gmail.com>
2022-02-24 13:24:03 -0500

    added random computer selection function

--- RPS.js
+++ RPS.js
@@ -1,1 +1,22 @@
-console.log("this")
+function computerPlay() {
+    let computerChoice = ""
+    let randomNumber = Math.floor(Math.random() * 3)
+    if (randomNumber == 0) {
+        computerChoice = "rock"
+    }
+    else if (randomNumber == 1) {
+        computerChoice = "paper"
+    }
+    else {
+        computerChoice = "scissors"
+    }
+    return computerChoice
+}
+
+console.log(computerPlay())
+console.log(computerPlay())
+console.log(computerPlay())
+console.log(computerPlay())
+console.log(computerPlay())
+console.log(computerPlay())
+console.log(computerPlay())
